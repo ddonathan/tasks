95c4bea6f1df4be7118ff07b8b4bc20e05becefc
Dan Donathan <dan@donathan.com>
2026-03-03 10:47:56 -0800

    fix: remove unused import

--- src/components/ProjectCombobox.tsx
+++ src/components/ProjectCombobox.tsx
@@ -1,7 +1,6 @@
 import { useMutation, useQuery } from "convex/react";
 import { useState } from "react";
 import { api } from "../../convex/_generated/api";
-import type { Id } from "../../convex/_generated/dataModel";
 
 interface ProjectComboboxProps {
   value: string;
